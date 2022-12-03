c55f9146a4187bbc5e2d830414f202b5a2f5f04a
Polina Koval <zagadka1316@gmail.com>
2022-12-03 12:30:56 +0300

    Generate Card in CardCreate.

--- discount_card/views.py
+++ discount_card/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views.generic import View
 
 from discount_card.forms import CardForm
+from discount_card.models import Card
 
 
 class CardCreate(View):
@@ -16,7 +17,11 @@ class CardCreate(View):
     def post(self, request):
         form = CardForm(request.POST)
         if form.is_valid():
-            # Do something with the form data
-            print(form.cleaned_data)
+            data = form.cleaned_data
+            for i in range(data["amount"]):
+                Card.generate_card(
+                    series=data["series"],
+                    validity_term=int(data["validity_term"]),
+                )
             return HttpResponseRedirect(reverse("card_create"))
         return render(request, self.template_name, {"form": form})
