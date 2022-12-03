fa865037d9c9fc38ac88419bae813040734e57f5
Polina Koval <zagadka1316@gmail.com>
2022-12-03 14:22:09 +0300

    Register SearchResultsView in urls.

--- discount_card/urls.py
+++ discount_card/urls.py
@@ -6,6 +6,7 @@ from discount_card.views import (
     CardDetail,
     CardListView,
     CardUpdateView,
+    SearchResultsView,
 )
 
 urlpatterns = [
@@ -14,4 +15,5 @@ urlpatterns = [
     path("card/<pk>/", CardDetail.as_view(), name="card_detail"),
     path("card/<pk>/delete/", CardDeleteView.as_view(), name="card_delete"),
     path("card/<pk>/update/", CardUpdateView.as_view(), name="card_update"),
+    path("search/", SearchResultsView.as_view(), name="search_results"),
 ]
