166fea1b2570f8135ff66fcaccdeb0d7b44e7a05
Polina Koval <zagadka1316@gmail.com>
2022-12-03 12:29:55 +0300

    Create staticmethods in Csrd model: generate_card_number, generate_card.

--- discount_card/models.py
+++ discount_card/models.py
@@ -1,4 +1,8 @@
+import random
+
+from dateutil.relativedelta import *
 from django.db import models
+from django.utils import timezone
 
 
 class Item(models.Model):
@@ -36,6 +40,21 @@ class Card(models.Model):
     status = models.CharField(
         max_length=30, choices=Statuses.choices, default=Statuses.INACTIVE
     )
+
+    @staticmethod
+    def generate_card_number():
+        return random.randint(1000000000, 9999999999)
+
+    @staticmethod
+    def generate_card(series, validity_term):
+        Card.objects.create(
+            series=series,
+            number=Card.generate_card_number(),
+            release_date=timezone.now(),
+            expiry_date=timezone.now() + relativedelta(months=+validity_term),
+            total_sum=0,
+            status=Card.Statuses.INACTIVE,
+        )
 
     def __str__(self):
         return f"{self.series}-{self.number}"
