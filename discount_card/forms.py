63b4b806ac1a8ffbc505ae905f9ff20d21ce81d9
Polina Koval <zagadka1316@gmail.com>
2022-12-03 12:35:13 +0300

    Add help_text and min_value in CardForm fields.

--- discount_card/forms.py
+++ discount_card/forms.py
@@ -8,6 +8,11 @@ class CardForm(forms.Form):
         (1, "1 MONTH"),
     )
 
-    series = forms.CharField(max_length=3, required=True)
-    amount = forms.IntegerField(required=True)
+    series = forms.CharField(
+        min_length=3,
+        max_length=3,
+        required=True,
+        help_text="Please, write 3 letters",
+    )
+    amount = forms.IntegerField(required=True, min_value=1)
     validity_term = forms.ChoiceField(choices=TERMS, required=True)
